3184ca1f83a8f7dbe7b9784fa22dc448d3ef2c2c
Aryan Singh <74867817+aryansingh920@users.noreply.github.com>
2022-08-31 03:12:12 +0530

    31/08

--- build/precache-manifest.938a08f7de9c0cd8df7974a061f611ee.js
+++ build/precache-manifest.938a08f7de9c0cd8df7974a061f611ee.js
@@ -1,30 +1,30 @@
 self.__precacheManifest = [
   {
-    "revision": "210faed342b7ec7a18f3",
+    "revision": "94f7105ca9c496249c0b",
     "url": "/cmrl_project/static/css/main.f2e844eb.chunk.css"
   },
   {
-    "revision": "210faed342b7ec7a18f3",
-    "url": "/cmrl_project/static/js/main.210faed3.chunk.js"
+    "revision": "94f7105ca9c496249c0b",
+    "url": "/cmrl_project/static/js/main.94f7105c.chunk.js"
   },
   {
     "revision": "56dbc343fe2dac40479d",
     "url": "/cmrl_project/static/js/1.56dbc343.chunk.js"
   },
   {
-    "revision": "d3ec2b80a8fb0b2066a3",
+    "revision": "9a0aa40a809131e26f06",
     "url": "/cmrl_project/static/css/2.0feba72a.chunk.css"
   },
   {
-    "revision": "d3ec2b80a8fb0b2066a3",
-    "url": "/cmrl_project/static/js/2.d3ec2b80.chunk.js"
+    "revision": "9a0aa40a809131e26f06",
+    "url": "/cmrl_project/static/js/2.9a0aa40a.chunk.js"
   },
   {
     "revision": "f09a56f3319bf1739f86",
     "url": "/cmrl_project/static/js/runtime~main.f09a56f3.js"
   },
   {
-    "revision": "7b820a2726b4508d944dff50a7b95a7a",
+    "revision": "eefec3e6fbfcb23c174d6ad2f131d0b7",
     "url": "/cmrl_project/index.html"
   }
 ];
