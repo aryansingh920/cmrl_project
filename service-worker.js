f748791099c8ad2178a1767dcef2c3025ef5d9fa
Aryan Singh <74867817+aryansingh920@users.noreply.github.com>
2022-08-31 03:07:01 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/cmrl_project/precache-manifest.9e3bf00d5cdfba92d7f839f1f72ac12b.js"
+  "/cmrl_project/precache-manifest.938a08f7de9c0cd8df7974a061f611ee.js"
 );
 
 workbox.clientsClaim();
